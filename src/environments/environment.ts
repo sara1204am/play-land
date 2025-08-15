d860388531826165ffa04c918071b60778737729
sara1204am <sara.acha@tutatordev.com>
2025-08-15 11:38:54 -0400

    Fix: backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@ import { environmentDefault } from './default';
 export const environment = {
   production: false,
   ...environmentDefault,
-  host: 'http://localhost:3000/api',
+  /* host: 'http://localhost:3000/api', */
+  host: 'https://play-land-backend-9cde6f1ea645.herokuapp.com/api',
   platformSeed: '9qxs2n3k',
   appVersion: 'v2.0.0',
   appName:'play_land',
