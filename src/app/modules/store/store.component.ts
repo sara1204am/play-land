1e71316bb34e3fe74a334b53b97a406dcc265828
sara1204am <sara.acha@tutatordev.com>
2026-04-08 10:04:37 -0400

    FIX: stules new version page

--- src/app/modules/store/store.component.ts
+++ src/app/modules/store/store.component.ts
@@ -59,6 +59,7 @@ export class StoreComponent implements OnInit {
   }
   categoryStyles: any = {
     juguete: 'bg-blue-100 text-blue-700 border-blue-300',
+    vehiculos: 'bg-orange-100 text-orange-700 border-orange-300',
     peluche: 'bg-pink-100 text-pink-700 border-pink-300',
     amigurumi: 'bg-purple-100 text-purple-700 border-purple-300',
     bebes: 'bg-emerald-100 text-emerald-700 border-emerald-300',
